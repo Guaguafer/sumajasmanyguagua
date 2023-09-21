7eaaaaa841fa1cf728051a9ddc35961c8ca2f95d
Guaguafer <145139458+Guaguafer@users.noreply.github.com>
2023-09-21 10:50:55 -0500

    Add files via upload

---
+++ suma2jasmanyguagua.cpp
@@ -0,0 +1,32 @@
+/*programa: suma.cpp
+autor: jasmany guagua
+fecha: 2023-09-14
+descripcion: suma dos numeros
+*/
+#include<iostream>
+using namespace std;
+float x,y,z;
+
+int ingreso()
+{
+	cout<<"ingrese x:";cin>>x;
+	cout<<"ingrese y:";cin>>y;
+	return (0);
+
+}
+
+int salida()
+
+{
+	cout<<"la suma de "<<x<<"+"<<y<<"="<<z<< endl;
+return (0);
+}
+
+int main()
+{
+	ingreso();
+	z=x+y;
+	salida();
+	return (0);
+}
+
